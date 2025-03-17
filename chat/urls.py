7f10cdb23c243a69023c3742e86fd97b67c2733e
KangIn <gangin0218@gmail.com>
2025-03-17 14:01:50 +0900

    feat: txt문서 저장 및 RAG연동 완료

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import chat_view
+from .views import chat_view, admin_page
 
 urlpatterns = [
-    path("", chat_view, name="chat"), # ✅ "chat/" → ""
+    path("", chat_view, name="chat"),
+path('admin-page/', admin_page, name='admin_page'),
 ]
