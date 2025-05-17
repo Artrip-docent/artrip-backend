481b04578e549ba1ebd7a0393cfba8a3d3bf9831
KangIn <gangin0218@gmail.com>
2025-05-17 11:22:26 +0900

    feat: 채팅내역 저장 및 get api

--- chat/mongo_utils.py
+++ chat/mongo_utils.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from django.conf import settings
-
+from datetime import datetime
 def get_mongo_collection():
     """
     MongoDB 컬렉션 객체를 반환합니다.
@@ -10,3 +10,29 @@ def get_mongo_collection():
     db = client[settings.MONGODB_DB]
     collection = db[settings.MONGODB_COLLECTION]
     return collection
+
+def save_chat_log(user_message, response_text, user_id=None, exhibition_id=None, artwork_id=None):
+    collection = get_mongo_collection()
+
+    history_entry = {
+        "question": user_message,
+        "answer": response_text
+    }
+
+    collection.update_one(
+        {
+            "user_id": user_id,
+            "exhibition_id": exhibition_id,
+            "artwork_id": artwork_id
+        },
+        {
+            "$push": {"history": history_entry},
+            "$set": {"time": datetime.utcnow()},
+            "$setOnInsert": {
+                "user_id": user_id,
+                "exhibition_id": exhibition_id,
+                "artwork_id": artwork_id
+            }
+        },
+        upsert=True
+    )
--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.chat_view, name="chat"),
+path("", views.chat_view, name="chat"),
 path('admin-page/', views.admin_page, name='admin_page'),
 path("delete_document/<int:doc_id>/", views.delete_document, name="delete_document"),
+path("history/", views.get_chat_history),
 ]
