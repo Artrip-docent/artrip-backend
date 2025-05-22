ea64072ac1acd41a9843f5abb0da95b4718ef322
rooftopSparkling <okok5317@naver.com>
2025-05-22 14:26:13 +0900

    fix: 전시회api수정

--- exhibition/views.py
+++ exhibition/views.py
@@ -1,15 +1,15 @@
 from django.http import JsonResponse
 from .models import Exhibition
 
-
 def exhibition_list(request):
     exhibitions = Exhibition.objects.all().values(
-        'title', 'start_date', 'end_date', 'location', 'image_url'
+        'id', 'title', 'start_date', 'end_date', 'location', 'image_url'
     )
 
     # 날짜를 문자열로 변환해서 반환
     data = [
         {
+            'id': e['id'],
             'title': e['title'],
             'period': f"{e['start_date']} ~ {e['end_date']}",
             'location': e['location'],
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets, permissions
+# reviews/views.py
+
+from rest_framework import viewsets
 from .models import Review
 from .serializers import ReviewSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        exhibition_id = self.request.query_params.get('exhibition')
+        if exhibition_id is not None:
+            queryset = queryset.filter(exhibition__id=exhibition_id)
+        return queryset
