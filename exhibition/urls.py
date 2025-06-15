fff8d7dcff8f9ccfe2c0e7cfd10242545e542c50
uni <cbcb9995@naver.com>
2025-06-15 09:03:30 +0900

    feat: 전시회 검색 API 구현

--- exhibition/urls.py
+++ exhibition/urls.py
@@ -3,10 +3,12 @@ from .views import (
     exhibition_list,
     exhibition_list_sorted_for_user,
     toggle_like,
+    search_exhibitions,
 )
 
 urlpatterns = [
     path('', exhibition_list, name='exhibition-list'),
-    path('sorted-user/', exhibition_list_sorted_for_user, name='exhibition-sorted-user'),  # 좋아요 우선정렬
+    path('sorted-user/', exhibition_list_sorted_for_user, name='exhibition-sorted-user'),  # 좋아요 누른 전시 우선정렬
     path('toggle-like/', toggle_like, name='toggle-like'),  # 좋아요 토글
+    path('search/', search_exhibitions, name='search-exhibitions'),  # 검색 기능
 ]
