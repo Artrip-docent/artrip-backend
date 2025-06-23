5f8b74d1626eb8a8770b368af9b8aa1a2493089e
rooftopSparkling <okok5317@naver.com>
2025-06-23 20:57:00 +0900

    fix: 프로필수정 버그수정

--- Artrip/settings.py
+++ Artrip/settings.py
@@ -193,4 +193,4 @@ MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION")
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
